5373ea610bc2ce102ea07e2d0ea83808cbdb8397
Ole Strohm <strohm99@gmail.com>
2024-12-01 10:21:57 +0000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Rust flake with nightly";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, flake-utils, rust-overlay, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = (import nixpkgs) {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.pkgsBuildHost.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-analyzer" "clippy" "rust-src" ];
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          nativeBuildInputs = [ rustToolchain ];
+        };
+      }
+    );
+}
