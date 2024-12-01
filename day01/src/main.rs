84a9e5d8f70b2d0dcec69f8cea20f92d12a05b55
Ole Strohm <strohm99@gmail.com>
2024-12-01 10:35:15 +0000

    Day 01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,32 @@
+use std::iter::zip;
+
+use itertools::Itertools;
+
 fn main() {
-    println!("Hello, world!");
+    //let input = include_str!("example.txt");
+    let input = include_str!("input.txt");
+
+    let (mut left, mut right): (Vec<_>, Vec<_>) = input
+        .lines()
+        .filter_map(|l| {
+            l.split_whitespace()
+                .filter_map(|n| n.parse::<u64>().ok())
+                .collect_tuple()
+        })
+        .unzip();
+
+    left.sort();
+    right.sort();
+
+    let part1 = zip(left.clone(), right.clone())
+        .map(|(l, r)| l.abs_diff(r))
+        .sum::<u64>();
+    println!("Part 1: {part1}");
+
+    let part2 = left
+        .into_iter()
+        .map(|l| l * right.iter().filter(|&&r| l == r).count() as u64)
+        .sum::<u64>();
+
+    println!("Part 2: {part2}");
 }
